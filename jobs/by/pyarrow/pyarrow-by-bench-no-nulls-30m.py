1d8a9c541dc782ccc01dd17bc52c18f95bd9104b
Cristian Ferretti <cristianferretti@deephaven.io>
2021-11-04 12:49:36 -0400

    Fixes for select-* versus not.

--- jobs/by/pyarrow/pyarrow-by-bench-no-nulls-30m.py
+++ jobs/by/pyarrow/pyarrow-by-bench-no-nulls-30m.py
@@ -1,9 +1,12 @@
 import pyarrow.dataset as ds
 import time
 
-df = ds.dataset(output_prefix_path + '/data/relation-no-nulls-30m.parquet', format='parquet').to_table().to_pandas()[['animal_id', 'adjective_id']]
+df = ds.dataset(output_prefix_path + '/data/relation-no-nulls-30m.parquet', format='parquet').to_table().to_pandas()
+
 start_time = time.time()
-count = len(df.groupby(['animal_id']).count().index)
+result = df[['animal_id', 'adjective_id']].groupby(['animal_id']).count()
+count = len(result.index)
 end_time = time.time()
+
 print(count)
 print(end_time - start_time)
--- jobs/select-by/pyarrow/pyarrow-select-by-bench-no-nulls-30m.py
+++ jobs/select-by/pyarrow/pyarrow-select-by-bench-no-nulls-30m.py
@@ -2,6 +2,7 @@ import pyarrow.dataset as ds
 import time
 
 df = ds.dataset(output_prefix_path + '/data/relation-no-nulls-30m.parquet', format='parquet').to_table().to_pandas()[['animal_id', 'adjective_id']]
+
 start_time = time.time()
 count = len(df.groupby(['animal_id']).count().index)
 end_time = time.time()
